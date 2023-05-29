89ee1c511dfbb99d4a55eda11771e885160207bf
unknown <jiawl20.len@gmail.com>
2023-05-30 00:42:55 +0800

    delete transcripts and change automated typing speed

--- javascript/typingAnimation.js
+++ javascript/typingAnimation.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     let typed = new Typed(".autoType", {
         strings: ["Software Development.", "Mobile Application.", "Game Development.","Coding."],
-        typeSpeed: 120,
-        backSpeed: 50,
+        typeSpeed: 60,
+        backSpeed: 35,
         loop: true
     })
 })
